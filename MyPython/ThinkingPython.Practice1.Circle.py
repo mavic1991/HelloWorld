3362ed8f99324005d7c4f497452ef3725f4141a3
mavic <wylygy@live.cn>
2020-06-26 07:02:22 +0800

    0.0.1

--- MyPython/ThinkingPython.Practice1.Circle.py
+++ MyPython/ThinkingPython.Practice1.Circle.py
@@ -6,9 +6,11 @@ def sequence(n):
         else:
             n=n*3+1
 
-def circle(x):
-    for i in range(6,x+1):
+def circle(start =1,end=2):
+    if start>end:start,end=end,start
+    for i in range(start,end+1):
         print('现在进入循环的是：{0}'.format(i))
         sequence(i)
 
-circle(10)
+if __name__=="__main__":
+    circle(20,25)
